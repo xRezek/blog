bdeffd606d2373a140a17ade8acb722a06c2462b
Mikołaj Maj <mmaj02@interia.pl>
2025-05-19 23:37:18 +0200

    adding initial files

---
+++ public/pages/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <title>Blog</title>
+</head>
+<body>
+
+<form action="signIn.php">
+  <label for="login">Login</label>
+  <input type="text" name="login">
+  <label for="password">Hasło</label>
+  <input type="password" name="password" id="">
+  <button type="button">Zaloguj</button>
+</form>
+  
+</body>
+</html>
